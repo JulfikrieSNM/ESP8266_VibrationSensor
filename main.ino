090f8103de00666afda291e539a8aca7a3ec3a37
JulfikrieSNM <joulezlucozade@gmail.com>
2025-09-16 07:50:50 +0800

    Create main.ino

---
+++ main.ino
@@ -0,0 +1,48 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include <UniversalTelegramBot.h>
+#include <ArduinoJson.h>
+
+// Replace with your WiFi credentials
+const char* ssid = "Redmi 15 5G";
+const char* password = "Jul12345";
+
+// Telegram bot details
+#define BOTtoken "8392570155:AAGM0tv4rWp3nf92a9s3zQT0kcqVXHHCOJo"   // From BotFather
+#define CHAT_ID "1127978350"      // From @userinfobot
+
+WiFiClientSecure client;
+UniversalTelegramBot bot(BOTtoken, client);
+
+const int vibrationPin = D2;  // Vibration sensor digital pin
+bool vibrationDetected = false;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(vibrationPin, INPUT);
+
+  // Connect to WiFi
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi connected");
+  client.setInsecure(); // Skip certificate check
+}
+
+void loop() {
+  int sensorValue = digitalRead(vibrationPin);
+
+  if (sensorValue == HIGH && !vibrationDetected) {
+    vibrationDetected = true;
+    Serial.println("⚠️ Vibration detected!");
+    bot.sendMessage(CHAT_ID, "⚠️ Vibration detected!", "");
+  } 
+  else if (sensorValue == LOW) {
+    vibrationDetected = false; // Reset
+  }
+
+  delay(500);
+}
